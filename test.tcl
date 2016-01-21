1beb8d2fb507bc4738566b20a683cd51c183aba5
Panagiotis Halatsakos MacHome <ditikos2@gmail.com>
2016-01-21 22:46:15 +0200

    First tcl parser

---
+++ test.tcl
@@ -0,0 +1,47 @@
+#!/usr/bin/tclsh
+
+package require csv
+package require struct::matrix
+
+::struct::matrix data
+
+namespace eval zubzub {
+	variable STAKE_FACTOR 
+}
+
+proc zubzub::init {} {
+
+	set chan [open data2.csv]
+	csv::read2matrix $chan data , auto
+	close $chan	
+
+	set rows [data rows]
+
+	set idx 0; #file cursor
+	set cust_id 0; # column:1
+
+	for {set row 0} {$row < $rows} {incr row} {
+		zubzub::populate_row $row cust_id [data get cell 1 $row]
+		zubzub::populate_row $row aud_op [data get cell 2 $row]
+		zubzub::populate_row $row prev_sf [data get cell 3 $row]
+		zubzub::populate_row $row curr_sf [data get cell 3 $row]
+	}
+
+}
+
+proc zubzub::populate_row { row key value } {
+	variable STAKE_FACTOR
+
+	set STAKE_FACTOR($row,$key) $value
+}
+
+proc zubzub::display_array {} {
+	variable STAKE_FACTOR
+
+	parray STAKE_FACTOR
+}
+
+
+zubzub::init
+zubzub::populate_row 8 curr_sf {1.00}
+zubzub::display_array
